8336747f9ab26191ac4085ce43be949c5a8ad59e
vladrph <54378636+vladrph@users.noreply.github.com>
2021-09-19 00:27:56 -0400

    Added comments

--- date.cpp
+++ date.cpp
@@ -33,7 +33,7 @@ Date::Date() : myDay(01), myMonth(01), myYear(0001) {
 
 //*************************************************************************************
 //Name:	Explicit Date constructor
-//Precondition: Unisgned obejcts are pointing to the local variables.
+//Precondition: Unisgned objects are pointing to the local variables.
 //Postcondition: Objects uses pointers to point to the myMonth, myDay, and myYear date object respectively
 //Description: This an explicit value constructor that set the date to a user defined date. The date prints 2-digits for
 // month (m) and day (d), and 4-digits for year (y). This function also sorts the day for specific parameters.
@@ -205,49 +205,3 @@ ostream &operator<<(ostream &out, Date &dateObj) {
 
 }
 
-//*********************************************************************************************
-//*********************************************************************************************
-//	D A T E	D R I V E R . C P P
-
-
-
-
-//EXAMPLE OF PROGRAM HEADER
-/********************************************************************************************
-
-Name:Vladimir Petit-Homme	                            Z#:Z15346795
-Course: Date Structures and Algorithm Analysis (COP3530) Professor: Dr. Petrie
-Due Date: 09/19/2021	Due Time: 11:59pm
-Total Points: 100 Assignment 3: Date program
-
-Description: This module implements date class and either returns the date or specific error message to the user.
-
-
-
-*********************************************************************************************/
-
-
-int main() {
-
-    Date myDate;
-    Date herDate(11, 14, 1953);
-    Date test1Date(25, 1, 1982); //should generate error message that bad month
-    Date test22Date(2, 29, 2020); //ok, should say leep year
-    Date test3Date(2, 30, 2021); //should generate error message that bad day
-    Date test4Date(1, 25, 0000); //should generate error message that bad year
-    Date test5Date(80, 40, 0000); //should generate error message that bad month, day and year
-
-    herDate.display();
-    //test22Date.display();
-    cout << herDate.getMonth() << endl;
-    cout << herDate.getDay() << endl;
-    cout << herDate.getYear() << endl;
-    myDate.setMonth(11);
-    myDate.setDay(12);
-    myDate.setYear(2015);
-    std::cout << "myDate: " << myDate << " test22Date: " << test22Date << " herDate: " << herDate << endl;
-    //cout << test22Date.display() << endl;
-
-    return 0;
-
-}
